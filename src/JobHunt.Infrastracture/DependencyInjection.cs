cb892b1d626bb5cdc22e92d164862456f1e45e49
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-03-19 00:30:42 +0200

    feat: configure infrastracture dependency injection

---
+++ src/JobHunt.Infrastracture/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using JobHunt.Application.Common.Interfaces;
+using JobHunt.Infrastracture.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobHunt.Infrastracture;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastracture(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IJobHuntDbContext, JobHuntDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        return services;
+    }
+}

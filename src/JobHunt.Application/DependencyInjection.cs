60c3c3856a8a988cd49a9843de035317b7716536
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-03-19 00:16:36 +0200

    feat: configure application dependency injection

---
+++ src/JobHunt.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JobHunt.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+        return services;
+    }
+}

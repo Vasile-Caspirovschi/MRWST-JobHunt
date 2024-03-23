dbcec681790c1511a135126daa212a29b2ec5887
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-03-23 13:17:54 +0200

    feat: define primary properties for entities

--- src/JobHunt.Domain/Entities/Company.cs
+++ src/JobHunt.Domain/Entities/Company.cs
@@ -2,4 +2,10 @@
 
 public class Company
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set;} = string.Empty;
+    public string? Phone { get; set;}
+    public AppUser Reprezentative = default!;
+    public string? Location { get; set; } = default!;
 }
--- src/JobHunt.Domain/Entities/JobPost.cs
+++ src/JobHunt.Domain/Entities/JobPost.cs
@@ -2,4 +2,12 @@
 
 public class JobPost
 {
+    public Guid JobId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int JobSalary { get; set; }
+    public JobCategory JobCategory { get; set; } = default!;
+    public Company Company { get; set; } = default!;
+    public string JobDescription { get; set; } = string.Empty;
+    public DateOnly PublishDate { get; set; }
+    public JobTypeType JobType { get; set; }
 }

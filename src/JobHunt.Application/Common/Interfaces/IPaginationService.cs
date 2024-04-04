743779916604f872f2034e2deeaff253a26ce663
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-04-04 13:37:11 +0300

    feat: add and register pagination service in infrastructure

---
+++ src/JobHunt.Application/Common/Interfaces/IPaginationService.cs
@@ -0,0 +1,19 @@
+﻿using CloudinaryDotNet.Actions;
+using JobHunt.Domain.Shared;
+using System.Linq.Expressions;
+
+namespace JobHunt.Application.Common.Interfaces;
+
+public interface IPaginationService<TEntity, TResult> where TEntity : class
+{
+    /// <summary>
+    /// Returns a PagedResult mapping at the same time form TEntity to TResult using specified selector
+    /// </summary>
+    /// <param name="selector">Used for mapping from source to result</param>
+    /// <param name="pageNumber"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="predicate">Used for filtering</param>
+    /// <returns></returns>
+    Task<PagedResult<TResult>> GetPagedAsync(int pageNumber, int pageSize,
+        Expression<Func<TEntity, TResult>> selector, Expression<Func<TEntity, bool>>? predicate = null);
+}

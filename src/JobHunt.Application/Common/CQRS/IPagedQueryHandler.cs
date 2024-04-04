b7e9fd55cb7d75cb89b5d96ccda77355c86d08bd
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2024-04-04 13:40:00 +0300

    feat: define cqrs IPagedQuery interface and its handler for working with PagedResult

---
+++ src/JobHunt.Application/Common/CQRS/IPagedQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿using JobHunt.Domain.Shared;
+using MediatR;
+
+namespace JobHunt.Application.Common.CQRS;
+
+public interface IPagedQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, PagedResult<TResponse>>
+    where TQuery : IPagedQuery<TResponse>;

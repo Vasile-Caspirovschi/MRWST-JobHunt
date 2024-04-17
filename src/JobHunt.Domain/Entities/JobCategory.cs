f9956742c22f4aa2c0fda79804c927abf6b85e96
CoteleaMarius <marius.cotelea1003@gmail.com>
2024-04-17 20:00:51 +0300

    feat: define entity relationships

--- src/JobHunt.Domain/Entities/JobCategory.cs
+++ src/JobHunt.Domain/Entities/JobCategory.cs
@@ -4,4 +4,6 @@ public class JobCategory
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public Guid JobPostId { get; set; }
+    public JobPost JobPost { get; set; } = default!;
 }

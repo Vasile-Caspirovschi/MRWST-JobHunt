e5928618cf5c154b9fe36139a71f36b88c02568f
CaimacanVladlena <vladushca2003@gmail.com>
2024-04-17 18:53:25 +0300

    feat: implement create job post command

---
+++ src/JobHunt.Application/Jobs/JobPostDto.cs
@@ -0,0 +1,16 @@
+﻿using JobHunt.Domain.Enums;
+
+namespace JobHunt.Application.Jobs;
+
+public class JobPostDto
+{
+    public Guid Id{ get; set; }
+    public required string Title { get; set; }
+    public required int JobSalary { get; set; }
+    public string JobCategoryName { get; set; } = string.Empty;
+    public required string Location { get; set; }
+    //public Company Company { get; set; } = default!;
+    public required string JobDescription { get; set; }
+    public required JobTypeType JobType { get; set; }
+    public required ExperienceRangeType Experience { get; set; }
+}

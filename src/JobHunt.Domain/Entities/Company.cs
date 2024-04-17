dc2e5db3178acd698a42dd40adcbe5eba016a7c9
CaimacanVladlena <vladushca2003@gmail.com>
2024-04-17 19:42:19 +0300

    feat: create a new company at register employer

--- src/JobHunt.Domain/Entities/Company.cs
+++ src/JobHunt.Domain/Entities/Company.cs
@@ -4,7 +4,7 @@ public class Company
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set;} = string.Empty;
+    public string? Description { get; set;} = string.Empty;
     public string? Phone { get; set;}
     public string CompanyRepresentativeId { get; set; } = default!;
     public AppUser CompanyRepresentative { get; set; } = default!;

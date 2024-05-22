1ddb6f0fac021db796916fba7beb8c4cc23155c7
Vasile-Caspirovschi <vasilica.caspirovschi@gmail.com>
2024-05-23 02:26:51 +0300

    fix:  minor bug fixes - remove name attribute from selects in jobpost and edit job post pages - add experience parsing and display experienc in job details page - small styling improvments for job filter in jobs page

--- src/JobHunt.Presentation/Models/JobViewModel.cs
+++ src/JobHunt.Presentation/Models/JobViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using JobHunt.Application.Companies;
 using JobHunt.Application.Jobs;
+using JobHunt.Domain.Enums;
 
 namespace JobHunt.Presentation.Models;
 
@@ -7,4 +8,19 @@ public class JobViewModel
 {
     public required JobPostDto JobPost { get; set; }
     public required CompanyDto Company { get; set; }
+
+    public string ParseExperience()
+    {
+        return JobPost.Experience switch
+        {
+            //ExperienceRangeType.LessThanOneYear => "Less than 1 year",
+            ExperienceRangeType.ZeroToOneYears => "Less than 1 year",
+            ExperienceRangeType.OneToTwoYears => "1-2 years",
+            ExperienceRangeType.TwoToThreeYears => "2-3 years",
+            ExperienceRangeType.ThreeToSixYears => "3-6 years",
+            ExperienceRangeType.SixOrMoreYears => "6+ years",
+            _ => "Not specified"
+        };
+    }
+
 }

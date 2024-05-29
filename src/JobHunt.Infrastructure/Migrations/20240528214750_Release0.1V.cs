a7622c33e376a23ce98493ac81664c6921bfaa0e
Vasile-Caspirovschi <vasilica.caspirovschi@gmail.com>
2024-05-29 15:17:11 +0300

    feat: configure connection string for remote azure sql server database

---
+++ src/JobHunt.Infrastructure/Migrations/20240528214750_Release0.1V.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobHunt.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Release01V : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
